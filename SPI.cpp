26fb3590b59a28d0dae7727a1c901cab291c17ab
Ahmed Eissa <eissaa80@gmail.com>
2013-09-25 23:25:58 +0200

    test
    test

---
+++ SPI.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2010 by Cristian Maglie <c.maglie@bug.st>
+ * SPI Master library for arduino.
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License version 2
+ * or the GNU Lesser General Public License version 2.1, both as
+ * published by the Free Software Foundation.
+ * Modified by Ahmed Eissa <eissaa80@gmail.com> for AVR8 soft processor @GADGET FACTORY
+ */
+
+
+#include "SPI.h"
+
+
+void SPIClass::begin(byte sck,byte mosi,byte miso,byte ss) {
+  // Set direction register for SCK and MOSI pin.
+  // MISO pin automatically overrides to INPUT.
+  // When the SS pin is set as OUTPUT, it can be used as
+  // a general purpose output port (it doesn't influence
+  // SPI operations).
+SCK  = sck;
+MOSI = mosi;
+MISO = miso;
+SS   = ss;
+
+
+    SPI1_mosi_LOC= MOSI;    //AL3 for Wing Slot AL
+    SPI1_miso_LOC= MISO;    //AL1 for Wing Slot AL
+    SPI1_sck_LOC = SCK;     //AL2 for Wing Slot AL
+    SPI1_cs_LOC  = SS;      //AL4 for Wing Slot AL
+
+  pinMode(SCK, OUTPUT);
+  pinMode(MOSI, OUTPUT);
+  pinMode(SS, OUTPUT);
+
+  digitalWrite(SCK, LOW);
+  digitalWrite(MOSI, LOW);
+  digitalWrite(SS, HIGH);
+
+  // Warning: if the SS pin ever becomes a LOW INPUT then SPI
+  // automatically switches to Slave, so the data direction of
+  // the SS pin MUST be kept as OUTPUT.
+  SPCR |= _BV(MSTR);
+  SPCR |= _BV(SPE);
+}
+
+void SPIClass::end() {
+  SPCR &= ~_BV(SPE);
+}
+
+void SPIClass::setBitOrder(uint8_t bitOrder)
+{
+  if(bitOrder == LSBFIRST) {
+    SPCR |= _BV(DORD);
+  } else {
+    SPCR &= ~(_BV(DORD));
+  }
+}
+
+void SPIClass::setDataMode(uint8_t mode)
+{
+  SPCR = (SPCR & ~SPI_MODE_MASK) | mode;
+}
+
+void SPIClass::setClockDivider(uint8_t rate)
+{
+  SPCR = (SPCR & ~SPI_CLOCK_MASK) | (rate & SPI_CLOCK_MASK);
+  SPSR = (SPSR & ~SPI_2XCLOCK_MASK) | ((rate >> 2) & SPI_2XCLOCK_MASK);
+}
+
